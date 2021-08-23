0037bcb88a3f06c6bec3599fa18fc50140b09da0
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-23 20:09:55 +0700

    log out

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -8,6 +8,10 @@ const MainNavigation = () => {
 
   const isLoggedIn = authContext.isLoggedIn;
 
+  const logoutHandler = () => {
+    authContext.logout();
+  };
+
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -27,7 +31,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
